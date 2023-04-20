6e8416c2c0a2ce9054164cb2ca8c30edde881e54
lucasnjsilva <lucasnathanj@gmail.com>
2023-04-20 18:26:52 -0300

    organização das rotas

--- start/routes/customer.routes.ts
+++ start/routes/customer.routes.ts
@@ -1,9 +1,20 @@
 import Route from '@ioc:Adonis/Core/Route';
 
-Route.get('/customers', 'CustomersController.index');
-Route.get('/customers/:id', 'CustomersController.show');
-Route.post('/customers', 'CustomersController.create');
-Route.put('/customers/:id', 'CustomersController.update');
-Route.patch('/customers/delete/:id', 'CustomersController.delete');
-Route.patch('/customers/restore/:id', 'CustomersController.restore');
-Route.delete('/customers/:id', 'CustomersController.destroy');
+// Rotas públicas
+Route.group(() => {
+  Route.post('/customers', 'CustomersController.create');
+});
+
+// Rotas privadas
+Route.group(() => {
+  Route.get('/customers/:id', 'CustomersController.show');
+  Route.put('/customers/:id', 'CustomersController.update');
+  Route.patch('/customers/delete/:id', 'CustomersController.delete');
+  Route.patch('/customers/restore/:id', 'CustomersController.restore');
+  Route.delete('/customers/:id', 'CustomersController.destroy');
+}).middleware(['auth', 'customerPermission']);
+
+// Rotas Protegidas (só precisa de autenticação)
+Route.group(() => {
+  Route.get('/customers', 'CustomersController.index');
+}).middleware('auth');
--- start/routes/provider.routes.ts
+++ start/routes/provider.routes.ts
@@ -1,9 +1,20 @@
 import Route from '@ioc:Adonis/Core/Route';
 
-Route.get('/providers', 'ProvidersController.index');
-Route.get('/providers/:id', 'ProvidersController.show');
-Route.post('/providers', 'ProvidersController.create');
-Route.put('/providers/:id', 'ProvidersController.update');
-Route.patch('/providers/delete/:id', 'ProvidersController.delete');
-Route.patch('/providers/restore/:id', 'ProvidersController.restore');
-Route.delete('/providers/:id', 'ProvidersController.destroy');
+// Rotas públicas
+Route.group(() => {
+  Route.post('/providers', 'ProvidersController.create');
+});
+
+// Rotas privadas
+Route.group(() => {
+  Route.get('/providers/:id', 'ProvidersController.show');
+  Route.put('/providers/:id', 'ProvidersController.update');
+  Route.patch('/providers/delete/:id', 'ProvidersController.delete');
+  Route.patch('/providers/restore/:id', 'ProvidersController.restore');
+  Route.delete('/providers/:id', 'ProvidersController.destroy');
+}).middleware(['auth', 'providerPermission']);
+
+// Rotas Protegidas (só precisa de autenticação)
+Route.group(() => {
+  Route.get('/providers', 'ProvidersController.index');
+}).middleware('auth');
